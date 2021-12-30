b4d25537a5288a48e190fd045392947567584fcf
Nadziraraa <nadziraazahra10@gmail.com>
2021-12-30 23:47:28 +0700

    Add project files.

---
+++ ServerCodeRest_20191040127_NadziraAzzahra/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Threading.Tasks;
+using ServiceRest_20190140127_NadziraAzzahra;
+
+namespace ServerCodeRest_20191040127_NadziraAzzahra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost hostObjek = null;
+            Uri address = new Uri("http://localhost:1907/Mahasiswa");
+            WebHttpBinding binding = new WebHttpBinding();
+
+            try
+            {
+                hostObjek = new ServiceHost(typeof(TI_UMY), address);
+                hostObjek.AddServiceEndpoint(typeof(ITI_UMY), binding, "");
+
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                hostObjek.Description.Behaviors.Add(smb);
+                Binding mexbinding = MetadataExchangeBindings.CreateMexHttpBinding();
+                hostObjek.AddServiceEndpoint(typeof(IMetadataExchange), mexbinding, "mex");
+
+                WebHttpBehavior whb = new WebHttpBehavior();
+                whb.HelpEnabled = true;
+                hostObjek.Description.Endpoints[0].EndpointBehaviors.Add(whb);
+
+                hostObjek.Open();
+                Console.WriteLine("Server ready ..");
+                Console.ReadLine();
+                hostObjek.Close();
+            }
+            catch (Exception ex)
+            {
+                hostObjek = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ ServerCodeRest_20191040127_NadziraAzzahra/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ServerCodeRest_20191040127_NadziraAzzahra")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ServerCodeRest_20191040127_NadziraAzzahra")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("e63a5f43-2572-4b09-b391-a0e7d5c8d290")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
